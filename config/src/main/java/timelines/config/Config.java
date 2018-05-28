1cdc84f15f7b6c3df755ec15c3f933f68f623136
Hinrich Kaestner <hinrich.kaestner@gmail.com>
2018-05-28 17:44:37 +0200

    use config without config.properties file

--- config/src/main/java/timelines/config/Config.java
+++ config/src/main/java/timelines/config/Config.java
@@ -1,44 +1,18 @@
 package timelines.config;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
 import java.util.Date;
-import java.util.Properties;
 
 /**
  * Used to access configuration values specified in the config.properties file
  */
 public class Config {
 
-    private static Properties properties;
-    private static String propertiesFile = "../config/src/main/resources/timelines/config/config.properties";
-
-    static {
-        try {
-            InputStream in = new FileInputStream(propertiesFile);
-            properties = new Properties();
-
-            properties.load(in);
-        } catch (Exception e) {
-            Path path = FileSystems.getDefault().getPath(".").toAbsolutePath();
-            System.out.println("Could not find the config file: '" + propertiesFile);
-            System.out.println("Current path is " + path.toString());
-            e.printStackTrace();
-        }
-    }
-
     public static String getDbPath() {
-        return properties.getProperty("db_path");
-    }
-
-    public static String getCachePath() {
-        return properties.getProperty("cache_path");
+        return "/mnt/ext-storage/res_";
+        // return "..\\timelines\\res_";
     }
 
     public static Date getStartDate() {
-        return new Date(Long.valueOf(properties.getProperty("start_date")));
+        return new Date(Long.valueOf(981020744966L));
     }
-
 }
